470c4d9de2644d20aba1725b993b24989da5d6bc
Fahad <fahad.zahid2004@gmail.com>
2025-03-22 22:38:42 +0000

    Hello world in java

---
+++ hello.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+
+    public static void main(String a[]) {
+
+        // Printing Hello World
+        System.out.println("Hello World ");
+
+        // Printing numbers
+        int num = 5;
+        int num2 = 5;
+        int result = num + num2;
+        System.out.println(result);
+    }
+}
